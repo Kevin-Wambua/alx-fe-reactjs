1d624a015c50bc7bc22f71efa990b7da4c74da1a
Kevin Wambua <766kevinwambua@gmail.com>
2025-07-13 21:06:44 +0300

    welcome message

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,6 +8,7 @@ import UserProfile from './components/UserProfile';
 function App() {
   return (
     <>
+      <WelcomeMessage />
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
