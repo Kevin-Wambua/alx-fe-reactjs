9e961e8c2733af779f804ecbec462d82cdee36bd
Kevin Wambua <766kevinwambua@gmail.com>
2025-07-20 13:41:52 +0300

    fixed userprofile

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,18 +4,21 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './components/Contact';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+     <><div>
+      <UserProfile name="Jane Doe" age={25} bio="Software Engineer from Nairobi." />
+    </div><Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router></>
   );
 }
 
