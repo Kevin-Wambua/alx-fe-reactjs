d6798455c87bb8f937175c7e4729380af64a0f85
Kevin Wambua <766kevinwambua@gmail.com>
2025-07-20 13:02:24 +0300

    React project 2

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function header() {
+    return (
+        <Header>
+            <h1 style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '20px' }}>My Favorite Cities</h1>
+        </Header>
+    );
+}
+
+export default header;
---
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+import WelcomeMessage from './components/WelcomeMessage';
+
+function WelcomeMessage() {
+    return (
+        <><WelcomeMessage /><div>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
+        </div></>
+    );
+}
+
+export default WelcomeMessage
