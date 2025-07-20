954c95efd95eccaa6673afb96606c18e3cfa8604
Kevin Wambua <766kevinwambua@gmail.com>
2025-07-20 13:34:24 +0300

    Add my-company website with routing and contact form

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', textAlign: 'center', marginTop: '20px' }}>
+      © {new Date().getFullYear()} My Company. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
